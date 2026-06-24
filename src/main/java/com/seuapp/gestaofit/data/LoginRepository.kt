a7563a616878d783372946a9354c0c098b11f5a3
thiago <thiagomuha@gmail.com>
2026-06-24 15:13:32 -0300

    NEW COMMIT

--- src/main/java/com/seuapp/gestaofit/data/LoginRepository.kt
+++ src/main/java/com/seuapp/gestaofit/data/LoginRepository.kt
@@ -1,7 +1,5 @@
 package com.seuapp.gestaofit.data
 
-import com.seuapp.gestaofit.data.model.LoggedInUser
-
 /**
  * Class that requests authentication and user information from the remote data source and
  * maintains an in-memory cache of login status and user credentials information.
